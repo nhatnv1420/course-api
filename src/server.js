c0f511871010bd3634b48a0b61a1180babd836b2
nhatnv1420 <nhatnv1420@gmail.com>
2021-12-03 09:06:56 +0700

    edit path in server.js file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-import jsonServer from 'json-server';
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("src/data/db.json");
+const router = jsonServer.router("./src/db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
 server.listen(port);
+
+console.log(port);
